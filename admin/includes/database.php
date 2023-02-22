de5d33bf9ce1c6327344d60c87900c5fa234b90a
Madiha Umair <madiha.altaf@yahoo.com>
2023-02-22 13:06:22 -0500

    first commit

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql108.epizy.com", // Host
+    "epiz_33373345", // Username
+    "jZNV39zwIrEH", // Password
+    "epiz_33373345_cms" // Database
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
